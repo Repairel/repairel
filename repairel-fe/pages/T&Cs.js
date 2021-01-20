66efbffc59ca5c74e404889101bef2dd5f085cdc
Gavin Lafferty <2386209l@student.gla.ac.uk>
2021-01-20 13:14:40 +0000

    Added repairel logo to the page, still need to connect to strapi

--- repairel-fe/pages/T&Cs.js
+++ repairel-fe/pages/T&Cs.js
@@ -2,7 +2,7 @@ import Header from "@components/header";
 import Head from "next/head";
 import Markdown from "markdown-to-jsx";
 import PropTypes from "prop-types";
-
+import Logo from '../../public/repairel-logo.svg';
 
 import Socials from "@components/socials";
 
@@ -16,10 +16,11 @@ const Terms = ({ content }) => {
         <title id="title">REPAIREL | Terms and Conditions</title>
       </Head>
       <Header />
-      <img
-        style={{ width: "100%", height: "auto" }}
+      <center><img
+        style={{ width: "25%", height: "25" }}
         src= {Logo}
-      ></img>
+      ></img></center>
+
       <section
         style={{ margin: "1rem", display: "flex", flexDirection: "column" }}
       >
