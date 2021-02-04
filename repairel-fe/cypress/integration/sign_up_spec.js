6e0597f72b191a49f33fa1ad55d47befcce9a294
Wafa Khan Tareen <2366702t@student.gla.ac.uk>
2021-02-04 19:02:18 +0500

    Added some test cases that were ommitted earlier

--- repairel-fe/cypress/integration/sign_up_spec.js
+++ repairel-fe/cypress/integration/sign_up_spec.js
@@ -50,25 +50,15 @@ describe ('The Registration Page',() => {
         cy.url().should('include', '/register')
         cy.contains('Please provide your email.')
     })
-    /**
+    
     it('Form not submitted when First Name is not provided', () => {
         cy.visit('/register')
         cy.get('input[name=surname]').type('Alice')
         cy.get('input[name=password]').type('1234')
         cy.get('input[name=email]').type('user@test.com')
-        cy.get('button').click()
+        cy.get('button').contains('REGISTER').click()
         cy.url().should('include', '/register')
     })
-    
-    it('Form not submitted when Surname is not provided', () => {
-        cy.visit('/register')
-        cy.get('input[name=forename]').type('Bob')
-        cy.get('input[name=password]').type('1234')
-        cy.get('input[name=email]').type('user@test.com')
-        cy.get('button').click()
-        cy.url().should('include', '/register')
-    })
-    */
 })
 
 describe ('Registration Form Looks OK',() => {
