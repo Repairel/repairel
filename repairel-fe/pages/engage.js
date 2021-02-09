080468dc89e6d9682164dd4bb65f692dfcb06101
Gavin Lafferty <2386209l@student.gla.ac.uk>
2021-02-09 13:21:26 +0000

    This commit is updating code to fetch infromation from strapi. Doesn't work yet https://stgit.dcs.gla.ac.uk/tp3-2020-CSP5/csp5-main/-/issues/89

--- repairel-fe/pages/engage.js
+++ repairel-fe/pages/engage.js
@@ -2,14 +2,14 @@ import Header from "@components/header";
 import Head from "next/head";
 import Markdown from "markdown-to-jsx";
 import PropTypes from "prop-types";
-import Logo from "../public/eco_feet.svg"
+import Logo from "../public/repairel-logo.svg"
 
 import Socials from "@components/socials";
 
 import { LinedHeading, StyledLink } from "../styles/global";
 import {Wishlist} from "@components/product/Product.style.js";
 
-const Engage = ({ content }) => {
+const Engage = ({content}) => {
   return (
     <>
       <Head>
@@ -17,16 +17,20 @@ const Engage = ({ content }) => {
       </Head>
       <Header />
       <img
-        style={{ width: "100%", height: "auto" }}
+        style={{ width: "50%", height: "50%" }}
         src= {Logo}
       ></img>
       <section
         style={{ margin: "1rem", display: "flex", flexDirection: "column" }}
       >
         <LinedHeading>Engage</LinedHeading>
-        <Markdown>{content[0].text}</Markdown> 
+        <p>
+          <Markdown>{content[0].Engage}</Markdown>
+        </p>
+        
       </section>
       <section style = {{margin:"5rem", display: "flex", justifyContent: "center"}}>
+            
             <Markdown>
                 This button will take you to the Just Giving homepage as a place holder
             </Markdown> 
@@ -46,11 +50,11 @@ const Engage = ({ content }) => {
 export async function getServerSideProps() {
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/engages`);
     const json = await res.json();
-    return { props: { content: json } };
+    return { props: {content: json} };
   }
   
-  Engage.propTypes = {
-    content: PropTypes.array,
-  };
-  export default Engage;
+Engage.propTypes = {
+  content: PropTypes.array,
+};
+export default Engage;
 
