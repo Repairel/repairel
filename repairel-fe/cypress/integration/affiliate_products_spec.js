2a97d7af833e588794f8c591904a0a902631f058
Wafa Khan Tareen <2366702t@student.gla.ac.uk>
2021-02-25 12:18:00 +0500

    Added tests for the Affiliate Product feature. Tests include checking that the Affiliate button is there, and that the ADD TO CART button is not there. The affiliate link has not been tested as it can change and thus break the tests.

---
+++ repairel-fe/cypress/integration/affiliate_products_spec.js
@@ -0,0 +1,13 @@
+describe ('The Affiliate Product feature',() => {
+    it ('Affiliate product has affiliate link button', () => {
+        cy.visit('/product/1')
+        cy.get('button').contains('I understand').click()
+        cy.contains('affiliate link')
+    })
+
+    it ("Affiliate product doesn't contain ADD to Basket button", () => {
+        cy.visit('/product/1')
+        cy.get('a').should('not.contain.text','ADD TO CART')
+    })
+})
+  
