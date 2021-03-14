0c23966dba29ef83d70e80c9efd9e7c3b727a2d1
Declan <2038796D@student.gla.ac.uk>
2021-03-14 12:59:06 +0000

    Re-enabled tests
    Re-enabled disabled tests.
    
    No issues closed.

--- repairel-fe/cypress/integration/disable_score_feature_spec.js
+++ repairel-fe/cypress/integration/disable_score_feature_spec.js
@@ -6,9 +6,7 @@ describe("Testing the removal of the score feature", function(){
     
     it("Check no rating on product page",function(){
         cy.visit("http://localhost:3000")
-        /* Commented out until further notice
         cy.get('.ProductListstyle__ProductImage-u9aoiu-1').click()
         cy.get('.ProductInfostyle__Rating-sc-2djmjb-3 feCNmP').should('not.exist')
-        */
     })
 })
--- repairel-fe/cypress/integration/product_sustainability_spec.js
+++ repairel-fe/cypress/integration/product_sustainability_spec.js
@@ -1,13 +1,11 @@
 describe ('The Product sustainability explanation',() => {
     it ('The explanation is displayed', () => {
         cy.visit('/product/1')
-        /* commented out until further notice
         cy.contains('DESIGN')
         cy.contains('RAW MATERIALS')
         cy.contains('MATERIAL MANUFACTURING')
         cy.contains('FOOTWEAR MANUFACTURING')
         cy.contains('USE')
         cy.contains('DISPOSAL')
-        */
     })
 })
