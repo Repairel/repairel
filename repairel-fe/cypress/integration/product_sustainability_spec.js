f3e26807ef9fd89bb0038d1d9eb19d846c165d83
Luke Holland <2342915h@student.gla.ac.uk>
2021-03-15 21:33:13 +0000

    Updated the tests to be more up to date
    Some tests were failing as they were outdated.

--- repairel-fe/cypress/integration/product_sustainability_spec.js
+++ repairel-fe/cypress/integration/product_sustainability_spec.js
@@ -1,11 +1,12 @@
 describe ('The Product sustainability explanation',() => {
     it ('The explanation is displayed', () => {
         cy.visit('/product/1')
-        cy.contains('DESIGN')
-        cy.contains('RAW MATERIALS')
-        cy.contains('MATERIAL MANUFACTURING')
-        cy.contains('FOOTWEAR MANUFACTURING')
-        cy.contains('USE')
-        cy.contains('DISPOSAL')
+        cy.contains('Design')
+        cy.contains('Raw Materials')
+        cy.contains('Material Manufacturing')
+        cy.contains('Footwear Manufacturing')
+        cy.contains('Retail')
+        cy.contains('Use')
+        cy.contains('Disposal')
     })
 })
