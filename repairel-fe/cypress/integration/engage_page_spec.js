93c021f77aaf5f4513395a8b76465dd501c3ab6a
Luke <2342915h@student.gla.ac.uk>
2021-03-20 11:44:05 +0000

    Syntax fix for the Engage page test Missed out a letter in the engage page test.

--- repairel-fe/cypress/integration/engage_page_spec.js
+++ repairel-fe/cypress/integration/engage_page_spec.js
@@ -1,6 +1,6 @@
 describe ('Test The Engage Page Loads Correctly', () => {
     it('Test The Engage Page Includes Text', () => {
         cy.visit('/engage')
-        y.get('h1').contains('ENGAGE')
+        cy.get('h1').contains('ENGAGE')
     })
 })
